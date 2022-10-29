b9409ace7912516ab162f44fa6476bfe926e50ba
iamsuteerth <suteerth@gmail.com>
2022-10-29 20:39:40 +0530

    Normal Commit

---
+++ src/edu/vit/corejava/hackerrank/PrimeChecker.java
@@ -0,0 +1,60 @@
+package edu.vit.corejava.hackerrank;
+
+/*
+ * @author Suteerth Subramaniam
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+import static java.lang.System.*;
+
+class Prime {
+    public void checkPrime(Integer... nums) {
+        for (Integer i : nums) {
+            BigInteger bi = new BigInteger(String.valueOf(i));
+            if (bi.isProbablePrime(1)) {
+                System.out.print(String.format("%s ", i));
+            }
+        }
+        System.out.println();
+    }
+}
+
+// Auto generated snippet
+public class PrimeChecker {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
